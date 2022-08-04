ee079810bace55bd4d6c1f214bf5474cddb2d02a
Vinicius_Red <atalibavinicius@gmail.com>
2022-08-04 17:07:41 -0300

    LibWindowsUpdate

---
+++ InstallServer/LibWindows.bat
@@ -0,0 +1,18 @@
+@echo
+call:IsAdmin
+Title Cs Source Server
+
+
+goto Check
+goto Update
+goto Download
+goto InstallServer
+
+:IsAdmin
+Reg.exe query "HKU\S-1-5-19\Environment"
+If Not %ERRORLEVEL% EQU 0 (
+ Cls & Echo You must have administrator rights to continue ... 
+ Pause & Exit
+)
+Cls
+goto:eof
