3d977b3847533541ee48b9e6018fd4dd04a6438e
Vinicius_Red <atalibavinicius@gmail.com>
2022-08-05 00:05:00 -0300

    Update

---
+++ InstallServer/LibWindows/LibWindows.bat
@@ -0,0 +1,11 @@
+@echo off
+echo --[For more information enter https://github.com/ViniciusRed/SourceServerDev]--
+setlocal
+for /f "delims==; tokens=1,2 eol=;" %%G in (config.cfg) do set %%G=%%H
+Title Cs Source Server
+
+goto Check
+goto Check Server
+goto Update
+goto Download
+goto InstallServer
