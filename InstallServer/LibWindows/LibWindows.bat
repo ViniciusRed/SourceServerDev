616da1b481f407dc5968ebf92d9e7f6a8e13dac9
Vinicius_Red <atalibavinicius@gmail.com>
2022-08-11 13:07:28 -0300

    Update

--- InstallServer/LibWindows/LibWindows.bat
+++ InstallServer/LibWindows/LibWindows.bat
@@ -4,9 +4,6 @@ setlocal
 for /f "delims==; tokens=1,2 eol=;" %%G in (config.cfg) do set %%G=%%H
 Title Cs Source Server
 
-:Download
-%SYSTEMROOT%\SYSTEM32\bitsadmin.exe /rawreturn /nowrap /transfer starter /dynamic /download /priority foreground %DownloadUrl% "%~dp0/%DownloadName%"
-pause
 goto Download
 goto Check
 goto Check Server
