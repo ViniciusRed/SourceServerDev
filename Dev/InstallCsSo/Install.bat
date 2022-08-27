29aae2dfc7a0cac069f4908c434c00fb2608234d
Vinicius_Red <atalibavinicius@gmail.com>
2022-08-27 00:49:52 -0300

    Update

--- Dev/InstallCsSo/Install.bat
+++ Dev/InstallCsSo/Install.bat
@@ -273,7 +273,7 @@ title Picking the download Key
 echo [Picking the download Key]
 %SYSTEMROOT%\SYSTEM32\bitsadmin.exe /rawreturn /nowrap /transfer starter /dynamic /download /priority foreground %KeyDownload% "%temp%\%Name5%"
 findstr %KeyUrl% %temp%\%Name5% >> %temp%\Key.cfg
-cscript %Name6% "%temp%\%Name7%" %Key%, %Key2%
+cscript %Name6% "%temp%\%Name7%" %Key% %Key2%
 for /f "delims==; tokens=1,2 eol=;" %%G in (%temp%\Key.cfg) do set %%G=%%H
 pause
 
